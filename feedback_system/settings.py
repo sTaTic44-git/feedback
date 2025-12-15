f2eb6f2ecb49d0a25cfccd11b7961d493e419c33
Prince Koundal <princekoundal44@gmail.com>
2025-12-15 10:22:28 +0530

    requirements and settings

--- feedback_system/settings.py
+++ feedback_system/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'feedback_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
